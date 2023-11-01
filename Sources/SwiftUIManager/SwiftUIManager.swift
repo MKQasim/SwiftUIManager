d223edd0b2d423a002d3dfe446052fcec1f1e28c
KamsQue <kamqasim1@gmail.com>
2023-11-01 03:11:11 +0100

    [Muhammad] added left and right View into NavBar

--- Sources/SwiftUIManager/SwiftUIManager.swift
+++ Sources/SwiftUIManager/SwiftUIManager.swift
@@ -46,4 +46,129 @@ extension View {
         }
     }
 }
+@available(iOS 13.0, *)
+struct IdentifiableNavigationItem: Identifiable {
+  let id = UUID()
+  let item: AnyView
+  
+  var view: AnyView {
+    item
+  }
+}
+@available(iOS 13.0, *)
+enum NavigationItemType {
+  case backButton(action: () -> Void)
+  case profileButton(action: () -> Void)
+  case moreButton(action: () -> Void)
+  case doubleTitleText(title: String ,  subTitle: String)
+  case customButton(view: AnyView)
+  case spacer
+  
+    @available(iOS 16.0, *)
+    var view: AnyView {
+    switch self {
+    case .backButton(let action):
+      return AnyView(
+        Button(action: action) {
+          Image("leftback")
+            .renderingMode(.template)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 32, height: 32)
+            .foregroundColor(.white)
+            .padding(.trailing,15)
+        }
+      )
+    case .profileButton(let action):
+      return AnyView(
+        Button(action: action) {
+          Image("navprofile")
+//            .renderingMode(.template)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 44, height: 44)
+//            .foregroundColor(.white)
+            .padding(.trailing,15)
+        }
+      )
+    case .moreButton(action: let action):
+      return AnyView(
+        Button(action: action) {
+          Image("more")
+            .renderingMode(.template)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 32, height: 32)
+            .foregroundColor(.white)
+        }
+      )
+    case .doubleTitleText(title:let title ,subTitle: let subTitle):
+      return AnyView(
+        VStack{
+          Text(title)
+            .frame(width: .infinity, height: 20,alignment: .leading)
+            .font(.system(size: 10))
+            .fontWeight(.bold)
+            .foregroundColor(Color.white)
+          
+          Text(subTitle )
+            .frame(width: .infinity, height: 20, alignment: .leading)
+            .font(.system(size: 12))
+            .fontWeight(.regular)
+            .foregroundColor(Color.white)
+        }
+      )
+    case .customButton(let view):
+      return view
+    case .spacer:
+      return AnyView(Spacer())
+    }
+  }
+}
 
+protocol NavigationItem {
+    @available(iOS 13.0, *)
+    var view: AnyView { get }
+}
+
+@available(iOS 13.0, *)
+struct LeftNavigationItem: NavigationItem {
+  let view: AnyView
+  
+    @available(iOS 16.0, *)
+    init(_ type: NavigationItemType, spacer: Bool = false) {
+    let itemView = type.view
+    let spacerView = spacer ? AnyView(Spacer()) : AnyView(EmptyView())
+    self.view = AnyView(HStack (spacing: 15){
+      spacerView
+      itemView
+    })
+  }
+}
+@available(iOS 13.0, *)
+struct RightNavigationItem: NavigationItem {
+  let view: AnyView
+  
+    @available(iOS 16.0, *)
+    init(_ type: NavigationItemType, spacer: Bool = false) {
+    let itemView = type.view
+    let spacerView = spacer ? AnyView(Spacer()) : AnyView(EmptyView())
+    self.view = AnyView(HStack {
+      itemView
+      spacerView
+    })
+  }
+}
+
+@available(iOS 13.0, *)
+struct NavigationTitleView: View {
+  let title: String
+  let color: Color
+  
+  var body: some View {
+    Text(title)
+      .frame(maxWidth: title.count == 0 ? .zero : .infinity)
+      .font(.headline)
+      .foregroundColor(color)
+  }
+}
