2e2aa934c716761e4d4c686d31a9b3db30a0c815
KamsQue <kamqasim1@gmail.com>
2023-11-01 02:48:41 +0100

    [Muhammad] Updated Custom Navbar

--- Sources/SwiftUIManager/SwiftUIManager.swift
+++ Sources/SwiftUIManager/SwiftUIManager.swift
@@ -2,13 +2,48 @@
 // https://docs.swift.org/swift-book
 import SwiftUI
 
-@available(iOS 13.0, *)
-extension View {
-    public func centeredHorizental() -> some View{
-        HStack{
-            Spacer()
-            self
-            Spacer()
+struct CustomNavigationBarView: ViewModifier {
+    var title: String
+
+    init(title: String) {
+        self.title = title
+    }
+
+    @available(iOS 13.0.0, *)
+    @available(macOS 10.15.0, *)
+    func body(content: Content) -> some View {
+        ZStack(alignment: .topLeading) { // Change alignment to .topLeading
+            content
+            VStack {
+                Text(title)
+                    .font(.body)
+                    .foregroundColor(.white)
+                    .padding()
+                Spacer()
+            }
+            .frame(maxWidth: .infinity, alignment: .topLeading)
+            .background(Color.blue.edgesIgnoringSafeArea(.top))
         }
     }
 }
+
+@available(iOS 13.0, *)
+@available(macOS 10.15, *)
+extension View {
+    func customNavigationBar<Content: View>(title: String, @ViewBuilder content: @escaping () -> Content) -> some View {
+        return ZStack(alignment: .top) {
+            content()
+            HStack {
+                Spacer()
+                Text(title)
+                    .font(.body)
+                    .foregroundColor(.white)
+                    .padding()
+                Spacer()
+            }
+            .frame(maxWidth: .infinity, alignment: .topLeading)
+            .background(Color.blue.edgesIgnoringSafeArea(.top))
+        }
+    }
+}
+
