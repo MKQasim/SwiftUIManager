af01e9446139f61d1068d1481d4d6fefb84d4cd2
KamsQue <kamqasim1@gmail.com>
2023-11-02 02:29:14 +0100

    no message

--- Sources/SwiftUIManager/SwiftUIManager.swift
+++ Sources/SwiftUIManager/SwiftUIManager.swift
@@ -1,19 +1,95 @@
 // MARK: - CustomNavigationBarView
+//
+//  CustomNavigationBar.swift
+//  HackerUITaskSwift
+//
+//  Created by KamsQue on 22/03/2023.
+//
+
 import SwiftUI
 import Swift
+
+
+// MARK: - IdentifiableNavigationItem
+
+@available(iOS 13.0, *)
+public struct IdentifiableNavigationItem: Identifiable {
+    public let id = UUID()
+    public let item: AnyView
+
+    public var view: AnyView {
+        item
+    }
+}
+
+// MARK: - NavigationAction
+@available(iOS 13.0, *)
+public protocol ControlAction {
+    var action: (() -> Void)? { get }
+}
+
+@available(iOS 13.0, *)
+public protocol NavigationControlButton {
+    var setImageOnControl: Image? { get }
+    var title: String? { get }
+    var color: Color? { get }
+}
+
+// MARK: - Navigation Item
+
+@available(iOS 13.0, *)
+public struct NavigationItem {
+    var content: () -> AnyView
+
+    public init(@ViewBuilder content: @escaping () -> AnyView) {
+        self.content = content
+    }
+}
+
+@available(iOS 13.0, *)
+public extension NavigationItem {
+    init<T: ControlAction & NavigationControlButton>(
+        _ item: T
+    ) {
+        self.content = {
+            if let action = item.action {
+                return AnyView(
+                    Button(action: action) {
+                        HStack {
+                            if let image = item.setImageOnControl {
+                                image
+                                    .renderingMode(.template)
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 32, height: 32)
+                                    .foregroundColor(item.color ?? .white)
+                            }
+                            if let title = item.title {
+                                Text(title)
+                                    .font(.headline)
+                            }
+                        }
+                    }
+                )
+            } else {
+                return AnyView(EmptyView())
+            }
+        }
+    }
+}
 
 @available(iOS 13.0, *)
 public struct CustomNavigationBarView: ViewModifier {
     public var title: String
     public var leftItems: [NavigationItem]
     public var rightItems: [NavigationItem]
-
+    
     public init(title: String, leftItems: [NavigationItem] = [], rightItems: [NavigationItem] = []) {
         self.title = title
         self.leftItems = leftItems
         self.rightItems = rightItems
     }
-
+    
     @ViewBuilder
     public func body(content: Content) -> some View {
         VStack(spacing: 0) {
@@ -29,152 +105,156 @@ public struct CustomNavigationBarView: ViewModifier {
             }
             .frame(maxWidth: .infinity, alignment: .topLeading)
             .background(Color.blue.edgesIgnoringSafeArea(.top))
-
+            
             content
         }
     }
-
+    
     public var leftItemsStack: some View {
         HStack(spacing: 16) {
-            ForEach(leftItems.map { IdentifiableNavigationItem(item: $0.view) }, id: \.id) { item in
+            ForEach(leftItems.map { IdentifiableNavigationItem(item: $0.content()) }, id: \.id) { item in
                 item.view
             }
         }
         .padding(.leading, 16)
     }
-
+    
     public var rightItemsStack: some View {
         HStack(spacing: 16) {
-            ForEach(rightItems.map { IdentifiableNavigationItem(item: $0.view) }, id: \.id) { item in
+            ForEach(rightItems.map { IdentifiableNavigationItem(item: $0.content()) }, id: \.id) { item in
                 item.view
             }
         }
         .padding(.trailing, 16)
     }
 }
-
 // MARK: - IdentifiableNavigationItem
 
 @available(iOS 13.0, *)
-public struct IdentifiableNavigationItem: Identifiable {
-    public let id = UUID()
-    public let item: AnyView
-
-    public var view: AnyView {
-        item
+public struct NavigationTitleView: View {
+    let title: String
+    let color: Color
+    
+    public init(title: String, color: Color) {
+        self.title = title
+        self.color = color
+    }
+    
+    public var body: some View {
+        Text(title)
+            .frame(maxWidth: title.count == 0 ? .zero : .infinity)
+            .font(.headline)
+            .foregroundColor(color)
     }
 }
 
-// MARK: - NavigationItemType
+@available(iOS 13.0, *)
+public struct CurvedShapeView: Shape {
+    let curveHeight: CGFloat
+    
+    public func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: .zero)
+        path.addLine(to: CGPoint(x: 0, y: curveHeight))
+        path.addQuadCurve(to: CGPoint(x: rect.width, y: curveHeight), control: CGPoint(x: rect.width / 2, y: curveHeight + 20))
+        path.addLine(to: CGPoint(x: rect.width, y: 0))
+        path.closeSubpath()
+        return path
+    }
+}
 
 @available(iOS 13.0, *)
-public enum NavigationItemType {
-    case backButton(action: () -> Void)
-    case profileButton(action: () -> Void)
-    case moreButton(action: () -> Void)
-    case doubleTitleText(title: String, subTitle: String)
-    case customButton(view: AnyView)
-    case spacer
-    @available(iOS 16.0, *)
-    public var view: AnyView {
-        switch self {
-            case .backButton(let action):
-                return AnyView(
-                    Button(action: action) {
-                        Image("leftback")
-                            .renderingMode(.template)
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 32, height: 32)
-                            .foregroundColor(.white)
-                            .padding(.trailing, 15)
-                    }
-                )
+public enum BackgroundStyle {
+    case color(Color)
+    case gradient(Gradient)
+}
 
-            case .profileButton(let action):
-                return AnyView(
-                    Button(action: action) {
-                        Image("navprofile")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 44, height: 44)
-                            .padding(.trailing, 15)
-                    }
-                )
-
-            case .moreButton(let action):
-                return AnyView(
-                    Button(action: action) {
-                        Image("more")
-                            .renderingMode(.template)
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 32, height: 32)
-                            .foregroundColor(.white)
-                    }
-                )
-
-            case .doubleTitleText(let title, let subTitle):
-                return AnyView(
-                    VStack {
+@available(iOS 13.0, *)
+public struct CurvedGradientNavigation<Content: View>: View {
+    var title: String
+    var titleColor: Color = .white
+    var backgroundColor: Color
+    var content: Content
+    var leftItems: [AnyView]?
+    var rightItems: [AnyView]?
+    var curveHeight: CGFloat = 50
+    var gradientColors: [Color]?
+    
+    init(title: String, backgroundColor: Color, @ViewBuilder content: () -> Content) {
+        self.title = title
+        self.backgroundColor = backgroundColor
+        self.content = content()
+    }
+    
+    public var body: some View {
+        ZStack {
+            backgroundColor.edgesIgnoringSafeArea(.all)
+            
+            VStack(spacing: 0) {
+                ZStack {
+                    CurvedShape(curveHeight: curveHeight)
+                        .fill(
+                            LinearGradient(
+                                gradient: Gradient(colors: gradientColors ?? [backgroundColor, backgroundColor.opacity(0.8)]),
+                                startPoint: .top,
+                                endPoint: .bottom
+                            )
+                        )
+                        .frame(height: curveHeight)
+                    
+                    HStack {
+                        if let leftItems = leftItems {
+                            HStack(spacing: 16) {
+                                ForEach(leftItems.indices, id: \.self) { index in
+                                    leftItems[index]
+                                }
+                            }
+                            .padding(.leading, 16)
+                        }
+                        
+                        Spacer()
+                        
                         Text(title)
-                            .frame(width: .infinity, height: 20, alignment: .leading)
-                            .font(.system(size: 10))
-                            .fontWeight(.bold)
-                            .foregroundColor(Color.white)
-
-                        Text(subTitle)
-                            .frame(width: .infinity, height: 20, alignment: .leading)
-                            .font(.system(size: 12))
-                            .fontWeight(.regular)
-                            .foregroundColor(Color.white)
+                        frame(width: title.count > 0 ? .infinity : .zero , height: 40)
+                            .font(.headline)
+                            .foregroundColor(titleColor)
+                            .padding(.top, curveHeight/2)
+                        
+                        Spacer()
+                        
+                        if let rightItems = rightItems {
+                            HStack(spacing: 16) {
+                                ForEach(rightItems.indices, id: \.self) { index in
+                                    rightItems[index]
+                                }
+                            }
+                            .padding(.trailing, 16)
+                        }
                     }
-                )
-            case .customButton(let view):
-                return view
-            case .spacer:
-                return AnyView(Spacer())
+                    .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top ?? 0)
+                    .padding(.horizontal, 16)
+                }
+                content
+            }
         }
     }
 }
 
-// MARK: - NavigationItem
-
-@available(iOS 13.0, *)
-public protocol NavigationItem {
-    var view: AnyView { get }
-}
-
-// MARK: - LeftNavigationItem
-
-@available(iOS 13.0, *)
-public struct LeftNavigationItem: NavigationItem {
-    public let view: AnyView
-
-    @available(iOS 16.0, *)
-    public init(_ type: NavigationItemType, spacer: Bool = false) {
-        let itemView = type.view
-        let spacerView = spacer ? AnyView(Spacer()) : AnyView(EmptyView())
-        self.view = AnyView(HStack(spacing: 15) {
-            spacerView
-            itemView
-        })
+public struct CurvedShape: Shape {
+    var curveHeight: CGFloat = 50
+    
+    @available(iOS 13.0, *)
+    public func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint(x: 0, y: rect.maxY))
+        path.addQuadCurve(to: CGPoint(x: rect.maxX, y: rect.maxY), control: CGPoint(x: rect.midX, y: rect.maxY - curveHeight))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+        path.addLine(to: CGPoint(x: 0, y: rect.minY))
+        path.closeSubpath()
+        return path
     }
 }
 
-// MARK: - RightNavigationItem
 
-@available(iOS 13.0, *)
-public struct RightNavigationItem: NavigationItem {
-    public let view: AnyView
 
-    @available(iOS 16.0, *)
-    public init(_ type: NavigationItemType, spacer: Bool = false) {
-        let itemView = type.view
-        let spacerView = spacer ? AnyView(Spacer()) : AnyView(EmptyView())
-        self.view = AnyView(HStack {
-            itemView
-            spacerView
-        })
-    }
-}
+
